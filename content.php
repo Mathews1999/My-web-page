a83d96f8eceee9a68ae777633a689c19f143127b
Mathews1999 <philipmathew399@gmail.com>
2021-03-10 15:18:03 +0530

    Project set

---
+++ content.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Content</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora saepe ab nobis quidem quod molestiae? Aspernatur officia aliquid nostrum, aliquam saepe exercitationem voluptas, necessitatibus architecto voluptatem magnam explicabo ratione inventore. </div>
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt explicabo numquam voluptate laudantium accusantium nobis sapiente cupiditate eligendi nemo magni debitis minima iure atque cumque, aliquid velit consequatur facere? Sunt!</div>
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde aliquam eos quaerat cum quod iusto, autem iste necessitatibus vero iure impedit aspernatur ipsum. Nobis id recusandae alias, ducimus porro culpa?</div>
+        </div>
+    </div>
+</body>
+</html>
